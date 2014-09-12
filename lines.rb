650d3c5528e1636b4b4c30b92a4bb908e7c188fd
Christopher Hinstorff <chris@chinstorff.com>
2014-09-12 13:31:18 -0700

    initial commit. pretty much done

---
+++ lines.rb
@@ -0,0 +1,25 @@
+require 'colorize'
+
+def text_to_lines (text)
+  ret = []
+  text.each_line "\n" do |line|
+    ret.push line
+  end
+
+  return ret
+end
+
+def run_lines (lines)
+  response = []
+  lines.each_with_index do |line, i|
+    prompt = "#{i}  "
+    
+    print prompt.red + line.red
+    print prompt
+    response.push $stdin.gets.chomp
+  end
+end
+
+text = File.read(ARGV.first).gsub(/\r\n?/, "\n")
+
+run_lines text_to_lines text
